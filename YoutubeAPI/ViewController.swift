cb3d2fa2e08d9219738d4f7b78391da96dc08bc4
John Smith <example@email.com>
2020-07-23 22:05:43 +0700

    Initial Commit

---
+++ YoutubeAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeAPI
+//
+//  Created by Đạt on 23/07/2020.
+//  Copyright © 2020 Đạt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ YoutubeAPITests/YoutubeAPITests.swift
@@ -0,0 +1,34 @@
+//
+//  YoutubeAPITests.swift
+//  YoutubeAPITests
+//
+//  Created by Đạt on 23/07/2020.
+//  Copyright © 2020 Đạt. All rights reserved.
+//
+
+import XCTest
+@testable import YoutubeAPI
+
+class YoutubeAPITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
